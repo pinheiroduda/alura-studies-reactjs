b4ed8bfceae220d25fad0eb79d3806487599da58
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-31 09:35:20 -0300

    feat: adds form style

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,13 +1,15 @@
 import { Button } from "../Button";
 
+import './style.scss'
+
 export function Form() {
   return(
-    <form>
-      <div>
+    <form className="novaTarefa">
+      <div className="inputContainer">
         <label htmlFor="task">Adicione um novo estudo</label>
         <input type="text" name="task" id="task" placeholder="O que você quer estudar?" required/>
       </div>
-      <div>
+      <div className="inputContainer">
         <label htmlFor="time">Tempo</label>
         <input type="time" step="1" name="time" id="00:00:00" max="01:30:00" required/>
       </div>
